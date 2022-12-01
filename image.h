3eb4a0c6ae67bc1e1575573ed8678dd6f888c624
gungler <50505991+gungler@users.noreply.github.com>
2022-12-01 21:19:48 +0800

    added rgb image and vector class

---
+++ image.h
@@ -0,0 +1,26 @@
+#ifndef _IMAGE_H_
+#define _IMAGE_H_
+
+#include <cmath>
+#include <string>
+#include <fstream>
+#include "rgb.h"
+using namespace std;
+class Image{
+public:
+    Image();
+    Image(int width, int height);
+    Image(int width, int height, rgb background);
+
+    bool set(int x, int y, const rgb& color);
+    void gammaCorrect(float gamma);
+    //output PPM image to 'out'
+    void writePPM(ostream& out);
+    void readPPM(string file_name);
+
+private:
+    rgb** raster;
+    int nx; //raster width
+    int ny; //raster height
+};
+#endif
